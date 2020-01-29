3a6740453f42f8c3d5c642f80e8b067ed9302542
geoffrey <any@any.fr>
2020-01-29 17:57:21 +0100

    Added tab

--- frontend/src/app/product-details/product-details.component.ts
+++ frontend/src/app/product-details/product-details.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  elements: any = [
+    {id: 1, first: 'Pouma R5', last: '54.99€', handle: '3'},
+    {id: 2, first: 'Nique V12', last: '119.99€', handle: '2'},
+    {id: 3, first: 'Abidas lvl 2', last: '89.99€', handle: '4'},
+  ];
 
+  headElements = ['ID', 'Nom', 'prix', 'Quantité'];
   ngOnInit() {
   }
 
